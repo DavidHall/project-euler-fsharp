c23d1390f330b103d854b09f7c892626bf3d70c6
David Hall <david.hall@deliveryhero.com>
2023-08-14 15:50:56 +0200

    solved euler 1

--- EulerSolutions.Tests/UnitTest1.fs
+++ EulerSolutions.Tests/UnitTest1.fs
@@ -7,7 +7,7 @@ open EulerSolutions
 type TestClass () = 
 
     [<Test>]
-    member this.IsMultipleOfThree_ReturnsTrue_ForThree() =
+    member thi.IsMultipleOfThree_ReturnsTrue_ForThree() =
         let resutl = EulerSolutions.EulerOne.isMultipleOfThree(3)
         Assert.IsTrue(resutl)
         
@@ -26,3 +26,20 @@ type TestClass () =
     member this.IsMultipleOfFive_ReturnsTrue_ForFive() =
         let resutl = EulerSolutions.EulerOne.isMultipleOfFive(5)
         Assert.IsTrue(resutl)
+
+    [<Test>]
+    member this.MainInt_When_N_Is_Zero_Returns_Sum() =
+        let sum = 5
+        let result = EulerSolutions.EulerOne.mainInt 0 sum
+        Assert.AreEqual(sum, result)
+
+    [<Test>]
+    member this.Main_When_N_Is_Ten_Returns_23() =
+        let result = EulerSolutions.EulerOne.main(9)
+        Assert.AreEqual(23, result)
+
+    [<Test>]
+    member this.Main_Euler() =
+        let result = EulerSolutions.EulerOne.main(233168)
+        Assert.AreEqual(23, result)
+
--- EulerSolutions/Library.fs
+++ EulerSolutions/Library.fs
@@ -7,3 +7,10 @@ module EulerOne =
     let isMultipleOfThree x = isMultipleOf x 3  
 
     let isMultipleOfFive x = isMultipleOf x 5 
+
+    let rec mainInt n sum: int =
+        if n = 0 then sum
+        else if isMultipleOfThree n || isMultipleOfFive n then mainInt (n-1) (n + sum)
+        else mainInt (n-1) (sum)
+
+    let main n = mainInt n 0 
